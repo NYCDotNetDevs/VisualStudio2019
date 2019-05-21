73f417ba3e10f8127159673cef3f69b8ebb201bd
Brian Jablonsky <bjablonsky@users.noreply.github.com>
2019-05-21 04:15:11 -0400

    Added Riverrun

---
+++ demos/GoT/Riverrun/HelloRiverrun.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Shared;
+
+namespace Riverrun
+{
+    public class HelloRiverrun : IHello
+    {
+        public void SayHello()
+        {
+            Console.WriteLine("Hello from Riverrun.");
+        }
+    }
+}
